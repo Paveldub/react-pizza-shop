858f3a855c88c232f4f280d161250686b2cdae78
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-05-13 21:26:59 +0300

    navigation

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Categories = () => {
+const Categories = ({ items, onClickItem }) => {
+
+  const [activeItem, setActiveItem] = useState(null);
+
+  const activeItemHandler = (index) => {
+    setActiveItem(index);
+  }
+  
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        <li
+          className={activeItem === null ? 'active' : ''}
+          onClick={() => activeItemHandler(null)}
+        >
+          Все
+        </li>
+        {items && items.map((name, index) => {
+          return (
+            <li
+              className={activeItem === index ? 'active' : ''}
+              onClick={() => activeItemHandler(index)}
+              key={`${name}_${index}`}
+            >
+              {name}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
 };
 
 export default Categories;
+ 
